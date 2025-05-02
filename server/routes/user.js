c3e1fbee90d04e1553e30d501399b26e06a782a6
beasroy <roybipasha676@gmail.com>
2025-05-02 17:01:10 +0530

    getUserbyId route fixed

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,6 @@ import { verifyAuth } from '../middleware/verifyAuth.js';
 const router = express.Router();
 
 router.post('/add-brand',verifyAuth,addBrandToUser);
-router.get('/getuser/:userId',verifyAuth,getUserById);
+router.get('/getuser/:userId',getUserById);
 
 export default router;

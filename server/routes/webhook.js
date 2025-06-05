26d13f023d2fb6093b89f4a128a3837714c4e1fa
beasroy <roybipasha676@gmail.com>
2025-06-05 11:34:04 +0530

    order cancel and shopify app uninstall webhoooks added

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import  {verifyWebhook, customersDataRequest , customersRedact , shopRedact, subscriptionUpdate } from '../webhooks/shopify.js';
-
-
+import { verifyWebhook, customersDataRequest, customersRedact, shopRedact, subscriptionUpdate, appUninstalled, orderCancelled } from '../webhooks/shopify.js';
 
 const router = express.Router();
 
 router.post('/customers/data_request', verifyWebhook, customersDataRequest);
 router.post('/customers/redact', verifyWebhook, customersRedact);
 router.post('/shop/redact', verifyWebhook, shopRedact);
-router.post('/app_subscriptions/update',verifyWebhook,subscriptionUpdate)
+router.post('/app_subscriptions/update', verifyWebhook, subscriptionUpdate);
+router.post('/app_uninstalled', verifyWebhook, appUninstalled);
+router.post('/orders/cancelled', verifyWebhook, orderCancelled);
 
 export default router;

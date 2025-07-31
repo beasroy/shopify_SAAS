159527a011c732d1acc9ef89c74e4e63bf2ca4b3
beasroy <roybipasha676@gmail.com>
2025-07-31 20:25:28 +0530

    updated the cors policy again

--- server/index.js
+++ server/index.js
@@ -47,9 +47,16 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 app.use(cors({
   origin: isDevelopment 
-    ? ['http://13.203.31.8', 'http://localhost:5173', 'https://extensions.shopifycdn.com']
-    : ['https://parallels.messold.com', 'https://extensions.shopifycdn.com'],
-  credentials: true  
+    ? true  // Allow all origins in development
+    : [
+        'https://parallels.messold.com',
+        'https://extensions.shopifycdn.com',
+        'https://*.shopifycdn.com',
+        'https://extensions.shopify.com'
+      ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json({

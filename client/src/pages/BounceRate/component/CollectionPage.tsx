7057a35432d3e9e43825f0bee5a9c38b3f494cdb
Raja28 <rajadavid03@gmail.com>
2026-01-08 19:40:30 +0530

    Fix: removed local filter function and used from parent component

--- client/src/pages/BounceRate/component/CollectionPage.tsx
+++ client/src/pages/BounceRate/component/CollectionPage.tsx
@@ -1,12 +1,11 @@
 import NewConversionTable from "@/pages/ConversionReportPage/components/ConversionTable";
 import { RootState } from "@/store";
 import { useSelector } from "react-redux";
+import { normalizePath } from "../BounceRatePage";
 
 interface ConversionComponentProps {
     isFullScreen: boolean;
-    currentFilter?: string[] | undefined;
     onDataUpdate: (data: any[], tabType: string) => void;
-    refreshTrigger?: number;
     tabData: Array<any>;
 }
 
@@ -16,29 +15,11 @@ const CollectionPage: React.FC<ConversionComponentProps> = ({ isFullScreen, tabD
     const primaryColumn = "All Page";
     const monthlyDataKey = "MonthlyData";
 
-    const normalizeCollectionPath = (path = "") => {
-        if (!path || path === "(not set)") return null;
-
-        // Remove query params & hash
-        let cleanPath = path.split("?")[0].split("#")[0];
-
-        // Remove locale prefix like /en-us, /fr, /de, etc.
-        // cleanPath = cleanPath.replace(/^\/[a-z]{2}(-[a-z]{2})?\//i, "/");
-
-        // Ensure it starts with /collections
-        // if (!cleanPath.startsWith("/collections")) return null;
-
-        const pathIncludesCollectiond = cleanPath?.split("/")?.includes("collections")
-
-        if (!pathIncludesCollectiond) return null
-
-        return cleanPath;
-    };
 
 
     const normalizedCollectionPages = tabData
         .map(item => {
-            const normalizedPath = normalizeCollectionPath(item["All Page"]);
+            const normalizedPath = normalizePath(item["All Page"], "collections");
             if (!normalizedPath) return null;
 
             return {
@@ -53,11 +34,9 @@ const CollectionPage: React.FC<ConversionComponentProps> = ({ isFullScreen, tabD
             <NewConversionTable
                 data={normalizedCollectionPages || []}
                 primaryColumn={primaryColumn}
-                // secondaryColumns={secondaryColumns}
                 monthlyDataKey={monthlyDataKey}
                 isFullScreen={isFullScreen}
                 locale={locale}
-                // filter={currentFilter}
             />
         </div>
     )

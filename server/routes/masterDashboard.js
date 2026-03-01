997b8fbb7ade5b25829302f929e74b51dfa42128
Raja28 <rajadavid03@gmail.com>
2026-03-01 20:47:42 +0530

    feat: Update masterDashboard routes to include new metrics endpoints and refactor existing routes for improved data handling

--- server/routes/masterDashboard.js
+++ server/routes/masterDashboard.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { fbAllBrandsAdData, getAllBrandsMetricsFromDB, getMarketingInsights } from '../controller/masterDashboard.js';
+import {
+    fbAllBrandsAdData, fetchFBAdAccountData,
+    fetchGoogleAdAndCampaignMetrics, fetchSalesSummary, getAllBrandsMetaSummary,
+    getAllBrandsMetricsFromDB, getBrandWiseFunnelMetrics, getMarketingInsights
+} from '../controller/masterDashboard.js';
 import { verifyAuth } from '../middleware/verifyAuth.js';
 const router = express.Router();
 
-router.get('/marketing-insights', verifyAuth, getMarketingInsights);
-router.get('/fb-ad-data', verifyAuth, fbAllBrandsAdData);
+// router.get('/marketing-insights', verifyAuth, getMarketingInsights);
+// router.get('/fb-ad-data', verifyAuth, fbAllBrandsAdData);
 router.get('/brand-metrics', verifyAuth, getAllBrandsMetricsFromDB);
-
-export default  router;
+router.get('/fb-ad-data', verifyAuth, fetchFBAdAccountData);
+router.get('/google-ad-data', fetchGoogleAdAndCampaignMetrics);
+router.get('/brand-funnel-metrics', getBrandWiseFunnelMetrics);
+router.get('/meta-sales-summary', fetchSalesSummary);
+export default router;
